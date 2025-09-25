6ff9e770f98035742e87347099f68632ffbd2d33
Edwin <edwinlobo0412@gmail.com>
2025-09-24 21:47:13 -0600

    Añadir validación para evitar líneas vacías en productos.txt

--- procesar_productos.rb
+++ procesar_productos.rb
@@ -1,9 +1,12 @@
 # procesar_productos.rb
-entrada = File.open("productos.txt", "r")
-salida = File.open("resultados.txt", "w")
 entrada.each_line do |linea|
-nombre, precio = linea.strip.split(",")
-precio = precio.to_f
+  next if linea.strip.empty?   # Saltar líneas vacías
+  nombre, precio = linea.strip.split(",")
+  precio = precio.to_f
+  precio_iva = precio * 1.15
+  salida.puts "#{nombre.ljust(10)} - Q#{'%.2f' % precio} - Q#{'%.2f' % precio_iva}"
+end
+
 
 2/2
 
