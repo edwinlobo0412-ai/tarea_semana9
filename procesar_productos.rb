c7de0b6a3c615bd4fbd5486c69c088ac94f59993
Edwin <edwinlobo0412@gmail.com>
2025-09-24 21:40:30 -0600

    Mejorar formato de salida en resultados.txt

--- procesar_productos.rb
+++ procesar_productos.rb
@@ -8,7 +8,7 @@ precio = precio.to_f
 2/2
 
 precio_iva = precio * 1.15
-salida.puts "#{nombre} - Q#{precio} - Q#{precio_iva.round(2)}"
+salida.puts "#{nombre.ljust(10)} - Q#{'%.2f' % precio} - Q#{'%.2f' % precio_iva}"
 end
 entrada.close
 salida.close
